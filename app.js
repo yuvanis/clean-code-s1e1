5463b610cbf119fd5f2e680c514e45032e1ec9a2
yuvanis <uvnistratov@gmail.com>
2022-05-27 13:46:14 +0300

    refactor: make selectors names laconic according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit-item";
 
-    deleteButton.className="delete";
+    deleteButton.className="del-item";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,7 +84,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".edit-item");
     var containsClass=listItem.classList.contains("todo-edit");
     //If class of the parent is .todo-edit
     if(containsClass){
@@ -156,8 +156,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.edit-item");
+    var deleteButton=taskListItem.querySelector("button.del-item");
 
 
     //Bind editTask to edit button.
